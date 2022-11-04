06cb6146f19716eab8e411f8ab1d1360adb65692
haifatanazefti <76585729+haifatanazefti@users.noreply.github.com>
2022-11-04 16:34:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const MoodRoute = require('./routes/MoodLogRoutes');
 app.use('/api', MoodRoute);
 
 // import  routes for the parents
- const parentRoutes = require('./routes/parentRoutes');
+ const parentRoutes = require('./routes/ParentRoutes');
 app.use('/api', parentRoutes);
 
 // import  routes for the child
